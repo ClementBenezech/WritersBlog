ef7cab95f3200a225b9bea5d3a06860182b0785f
Clement Benezech <clement.benezech@gmail.com>
2021-04-25 21:50:55 +0200

    several improvements to JS, animated the book thumbnails

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const strapiURL = "https://cbd-strapi.herokuapp.com/";
 
-function createDivIn($domLocation, $divId, $tag)
+function createDivIn($domLocation, $divId, $tag, $class)
 {
 	div = document.createElement($tag); //on crée un nouveau div
 	div.setAttribute("id", $divId); //On assigne l'ID au DIV nouvellement créé
+	div.setAttribute("class", $class); //On assigne sa classe au DIV nouvellement créé
 	div = $domLocation.appendChild(div); //On crée le Div en tant qu'enfant de la section "getelementbyid div1"
 	return div;
 }
@@ -20,18 +21,12 @@ fetch("https://cbd-strapi.herokuapp.com/aw-books")  //appel de l'api
 				data = response[i]; //Dans une variable data, on met le Ième enregistrement renvoyé par l'API
 				currentpost = "post"+i; //on définit la classe à donner à l'élément pour son positionnement dans la grid.
 
-				postContainer = createDivIn(document.getElementById("blogContent"), currentpost, "div");
-				postContainer.setAttribute("class", "bookgrid__element bookgrid__element--"+i);
-				postTitle = createDivIn(postContainer, "title", "div");
-				postTitle.setAttribute("class", "bookgrid__element__title");
-				postPublisher = createDivIn(postContainer, "publisher", "div");
-				postPublisher.setAttribute("class", "bookgrid__element__publisher");
-				postReleaseDate = createDivIn(postContainer, "date", "div");
-				postReleaseDate.setAttribute("class", "bookgrid__element__date");
-				postImage = createDivIn(postContainer, "image", "img");
-				postImage.setAttribute("class", "bookgrid__element__image");
-				postContent = createDivIn(postContainer, "synopsis", "div");
-				postContent.setAttribute("class", "bookgrid__element__synopsis");
+				postContainer = createDivIn(document.getElementById("bookgrid"), currentpost, "div", "bookgrid__element bookgrid__element--"+i);
+				postTitle = createDivIn(postContainer, "title", "div", "bookgrid__element__title");
+				postPublisher = createDivIn(postContainer, "publisher", "div", "bookgrid__element__publisher");
+				postReleaseDate = createDivIn(postContainer, "date", "div", "bookgrid__element__date");
+				postImage = createDivIn(postContainer, "image", "img", "bookgrid__element__image");
+				postContent = createDivIn(postContainer, "synopsis", "div", "bookgrid__element__synopsis");
 				
 				//On insère le src de la balise image
 				postImage.src = data['images'][0]['url'];
