25bb055aec4229b3b79f0cb4d2b7dc5cf2a01296
Clement Benezech <clement.benezech@gmail.com>
2021-04-27 21:17:01 +0200

    added full page blog post display

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,6 @@
 const strapiURL = 'https://cbd-strapi.herokuapp.com/';
+let postContainer = new Array(20);
+let i = 0;
 
 function createDivIn($domLocation, $divId, $tag, $class)
 		{
@@ -19,6 +21,57 @@ function createTitle($contentType, $icon) {
 	sectionTitle.innerHTML = $icon+"<h2>"+$contentType+"</h2>";
 	
 
+}
+
+function getBlogPost($id) {
+
+	console.log("t'as cliqué mon buddy"+$id);
+	console.log(strapiURL+"aw-blogposts/"+$id);
+	fetch(strapiURL+"aw-blogposts/"+$id)  //appel de l'api
+		.then (response =>  // Si on reçoit une réponse
+			{
+				response.json() //On la transforme en json mais c'est toujours une promise j'ai pas compris pourquoi
+				.then (response => //Donc, on refait un then
+					{
+
+						deleteMainContent();
+						createTitle("Blog", "<i class='fas fa-feather-alt'></i>");
+						
+						data = response; //Dans une variable data, on met l'enregistrement renvoyé par l'api
+						console.log(data);
+						postContainer = createDivIn(document.getElementById("main-content"), "post", "div", "blog__element");
+
+						postDateTag = createDivIn(postContainer, "date_tag", "div", "blog__element__date");
+						postImage = createDivIn(postContainer, "image", "img", "blog__element__image");
+						postTitle = createDivIn(postContainer, "title", "div", "blog__element__title");
+						postContent = createDivIn(postContainer, "text", "div", "blog__element__synopsis");
+						
+						//On insère le src de la balise image
+						postImage.src = data['images'][0]['url'];
+						
+
+						/*On insère le contenu renvoyé par l'api dans les divs apropriés.*/
+						postTitle.textContent = data['title'];
+						console.log(data['text'].substring(0,10));
+						postContent.textContent = (data['text'])+" ...";
+						postDateTag.textContent = data['tag']+" / "+data['published_at'].substring(0,10);
+						console.log(data['published_at'].substring(0,10));
+
+
+						
+					})
+	
+			})
+
+
+}
+
+function aboutContent (){
+	deleteMainContent();
+	createTitle("A propos de l'auteure", "<i class='fas fa-user-astronaut'></i>");
+
+		postContainer = createDivIn(document.getElementById("main-content"), "authorContainer", "div", "author__container");
+		postContainer.innerHTML= "<img src='public/images/AW_profile.jpg'><p>Anne Waddington, auteure prolifique, partage son temps entre Toulouse et les Monts de Lacaune. </p><p> Elle aime revisiter l'Histoire et mettre en lumière, tant dans ses romans que dans ses thrillers, des éléments du passé dont les manuels évitent de parler . </p><p>Son style enlevé, la justesse de ses personnages et sa façon de distiller le suspens vous emportent jusqu'à des dénouements saisissants et inattendus. </p><p>Anciennement psychologue et thérapeute familial, la retraite lui permet de se consacrer à l'écriture et au théâtre.</p>";
 }
 
 
@@ -79,45 +132,44 @@ function getPostList() {
 					/*console.log("contenu de response: "+response);*/
 					for (i = 0; i < Object.keys(response).length; i++)
 					{
-						data = response[i]; //Dans une variable data, on met le Ième enregistrement renvoyé par l'API
-						currentpost = "post"+i; //on définit la classe à donner à l'élément pour son positionnement dans la grid.
-						console.log(data);
-						postContainer = createDivIn(document.getElementById("main-content"), currentpost, "div", "blog__element blog__element--"+i);
+						const data = response[i]; //Dans une variable data, on met le Ième enregistrement renvoyé par l'API
+						/*currentpost = "post"+i; //on définit la classe à donner à l'élément pour son positionnement dans la grid.*/
+						
+						postContainer = createDivIn(document.getElementById("main-content"), data['id'], "div", "blog__element blog__element--"+i);
 
 						postDateTag = createDivIn(postContainer, "date_tag", "div", "blog__element__date");
 						postImage = createDivIn(postContainer, "image", "img", "blog__element__image");
 						postTitle = createDivIn(postContainer, "title", "div", "blog__element__title");
 						postContent = createDivIn(postContainer, "text", "div", "blog__element__synopsis");
+
+						
 						
 						//On insère le src de la balise image
 						postImage.src = data['images'][0]['url'];
-						
-
 						/*On insère le contenu renvoyé par l'api dans les divs apropriés.*/
 						postTitle.textContent = data['title'];
-						console.log(data['text'].substring(0,10));
 						postContent.textContent = (data['text'].substring(0,500))+" ...";
 						postDateTag.textContent = data['tag']+" / "+data['published_at'].substring(0,10);
-						console.log(data['published_at'].substring(0,10));
+
+						console.log(postContainer.id)
+						
+						/*postContainer.onclick = function() {;};*/
+
+						postContainer.addEventListener("click", function() {getBlogPost(this.id)});
+						
+						
+
+						
 					};
 				})
 			})
 }
 
-function aboutContent (){
-	deleteMainContent();
-	createTitle("A propos de l'auteure", "<i class='fas fa-user-astronaut'></i>");
-
-		postContainer = createDivIn(document.getElementById("main-content"), "authorContainer", "div", "author__container");
-		postContainer.innerHTML= "<img src='public/images/AW_profile.jpg'><p>Anne Waddington, auteure prolifique, partage son temps entre Toulouse et les Monts de Lacaune. </p><p> Elle aime revisiter l'Histoire et mettre en lumière, tant dans ses romans que dans ses thrillers, des éléments du passé dont les manuels évitent de parler . </p><p>Son style enlevé, la justesse de ses personnages et sa façon de distiller le suspens vous emportent jusqu'à des dénouements saisissants et inattendus. </p><p>Anciennement psychologue et thérapeute familial, la retraite lui permet de se consacrer à l'écriture et au théâtre.</p>";
-}
-
-
 
 
 window.onload = function(){
 	getPostList();
-}
+};
 
 document.getElementById('blog-posts').onclick = function () {
 	getPostList();
@@ -129,5 +181,5 @@ document.getElementById('book-list').onclick = function () {
 
 document.getElementById('author').onclick = function () {
 	aboutContent();
-}
+};
 	  
